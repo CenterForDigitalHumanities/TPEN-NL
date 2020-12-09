72be9cfce7e6b8a3b5401cf76f152183f3f5e582
Patrick Cuba <cubap@slu.edu>
2020-12-09 16:35:31 -0600

    care less

--- web/js/newberry.js
+++ web/js/newberry.js
@@ -5993,7 +5993,7 @@ document.addEventListener("DOMContentLoaded",()=>{
  * NL instanceof Map || document.body.append(document.createElement("script").setAttribute("src", /italian/i.test(document.title) ? "https://centerfordigitalhumanities.github.io/Newberry-Italian-paleography/www/script/templates.js" : "https://centerfordigitalhumanities.github.io/Newberry-French-paleography/www/script/templates.js"))
  */
 
-    if (!imgTop){ // prevent on transcription interfaces
+    if (!window['imgTop']){ // prevent on transcription interfaces
       const h = document.querySelector("body>header")
       const f = document.querySelector("body>footer")
       if (h === null) {
